c319a9456b92c72285e92c75ca5732153ae11579
Julianocarvalho <julianocarvalho128@gmail.com>
2018-01-04 13:49:53 -0200

    Criando novo model Phones

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,13 @@
 class Contact < ApplicationRecord
+	#Associations
 	belongs_to :kind
+	has_many :phones
 
 	def as_json(options={})
-		super(root: true)
+		super(
+			root: true,
+			include: {kind: {only: :description}}
+
+			)
 	end
 end
