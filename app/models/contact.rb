b125479c4cde09b8de7957d327fd75b2dad69502
Julianocarvalho <julianocarvalho128@gmail.com>
2018-01-04 16:29:48 -0200

    crud com has_one

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,9 +3,9 @@ class Contact < ApplicationRecord
 	belongs_to :kind
 	has_many :phones
 	has_one :address
-	
+
 	accepts_nested_attributes_for :phones, allow_destroy: true
-	accepts_nested_attributes_for :address
+	accepts_nested_attributes_for :address, update_only: true
 
 
 end
