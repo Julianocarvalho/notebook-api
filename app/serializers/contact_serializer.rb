8c418b5028297547f0bc7a41a1f940304b4ec506
Julianocarvalho <julianocarvalho128@gmail.com>
2018-01-08 21:40:58 -0200

    add link e correções

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,6 +1,16 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
+  belongs_to :kind do
+    link(:related) {kind_url(object.kind.id)}
+  end
+
+  has_many :phones
+  has_one :address
+
+#  link(:self) {contact_url(object.id)}
+#  link(:kind) {kind_url(object.kind.id)}
+
   def attributes(*args)
   	h = super(*args)
   	# pt-BR ----> h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
